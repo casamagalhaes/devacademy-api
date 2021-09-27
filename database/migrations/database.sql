e8037d74a12ecf291f67ddacabdd88d57f733942
Ellyo Freitas <ellyofreitas13@gmail.com>
2021-09-26 21:28:30 -0300

    refactor database migration

--- database/migrations/database.sql
+++ database/migrations/database.sql
@@ -7,8 +7,7 @@ CREATE TABLE IF NOT EXISTS `products` (
 
 GO;
 
-INSERT INTO products (`id`, `name`, `price`) VALUES 
+INSERT OR IGNORE INTO products (`id`, `name`, `price`) VALUES
 ('a716da8a-0966-4cd4-af36-51ad068ad68a', 'COCA-COLA LATA 350ML', 6.99),
 ('c1e19626-167e-4b23-87eb-c28c3bfea86f', 'SKOL LATA 350ML', 1.60),
-('c254557d-3890-483b-a7ca-123be9f84f2a', 'BISCOITO AMORI CHOCOLATE 140G', 2.19)
-ON DUPLICATE KEY UPDATE `id`=values(`id`), `name`=values(`name`), `price`=values(`price`)
+('c254557d-3890-483b-a7ca-123be9f84f2a', 'BISCOITO AMORI CHOCOLATE 140G', 2.19);
