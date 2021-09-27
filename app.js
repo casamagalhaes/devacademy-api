58294937e43f9b1d493f262b6e076de14b9228fa
Ellyo Freitas <ellyofreitas13@gmail.com>
2021-09-26 22:15:41 -0300

    logging server error

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use('/products', productsRouter);
 app.use((err, _, res, next) => {
   if (err instanceof ApiError)
     return res.status(err.statusCode).json({ message: err.message });
+  console.error(err);
   return res.status(500);
 });
 
